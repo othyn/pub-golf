4ca22182f3e8db303ca79a17df1d69426e8ba3c0
Ben <git@othyn.com>
2018-03-24 17:46:20 +0000

    Separate out nav

--- resources/views/join.blade.php
+++ resources/views/join.blade.php
@@ -3,42 +3,11 @@
 @section ('content')
 
 <section class="hero is-info is-fullheight">
+
     <div class="hero-head">
-      <nav class="navbar">
-        <div class="container">
-          <div class="navbar-brand">
-            <a class="navbar-item" href="../">
-              <img src="http://bulma.io/images/bulma-type-white.png" alt="Logo">
-            </a>
-            <span class="navbar-burger burger" data-target="navbarMenu">
-              <span></span>
-              <span></span>
-              <span></span>
-            </span>
-          </div>
-          <div id="navbarMenu" class="navbar-menu">
-            <div class="navbar-end">
-              <a class="navbar-item is-active">
-                Home
-              </a>
-              <a class="navbar-item">
-                Examples
-              </a>
-              <a class="navbar-item">
-                Documentation
-              </a>
-              <span class="navbar-item">
-                <a class="button is-white is-outlined is-small" href="https://github.com/dansup/bulma-templates/blob/master/templates/landing.html">
-                  <span class="icon">
-                    <i class="fa fa-github"></i>
-                  </span>
-                  <span>View Source</span>
-                </a>
-              </span>
-            </div>
-          </div>
-        </div>
-      </nav>
+
+        @include ('layouts.nav')
+
     </div>
 
     <div class="hero-body">
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar">
+
+    <div class="container">
+
+        <div class="navbar-brand">
+
+            <a class="navbar-item" href="/">Pub Golf</a>
+
+            <span class="navbar-burger burger" data-target="navbarMenu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+
+        </div>
+
+        <div id="navbarMenu" class="navbar-menu">
+
+            <div class="navbar-end">
+
+                <a class="navbar-item" href="/">New / Join game</a>
+
+                <span class="navbar-item">
+                    <a class="button is-white is-outlined is-small" href="https://github.com/othyn/pub-golf">
+                        <span class="icon">
+                            <i class="fa fa-github"></i>
+                        </span>
+                        <span>View Source</span>
+                    </a>
+                </span>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</nav>
