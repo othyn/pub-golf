40151c893fbc1920c7294b4c866e25d92f6c0b0e
Ben <github@othyn.com>
2018-03-25 21:54:28 +0100

    Game name in title position on play

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -2,8 +2,8 @@
 
 @section ('hero-content')
 
-    <h1 class="title is-fancy-font is-size-0">Scorecard</h1>
-    <h4 class="subtitle p-b-md">Let's see how the game's going...</h4>
+    <h1 class="title is-fancy-font is-size-0">{{ $game_name }}</h1>
+    <h4 class="subtitle p-b-md">Keep your score &amp; see how the game's going...</h4>
 
 @endsection
 
