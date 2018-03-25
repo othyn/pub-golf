7bea24e9be3547accc4e2faa1120b02d181afac9
Ben <github@othyn.com>
2018-03-25 22:09:48 +0100

    Scaffold temporary prototyping data

--- routes/web.php
+++ routes/web.php
@@ -11,54 +11,83 @@
 |
 */
 
+define('TEMP_holes', [
+    [
+        'location' => 'Pub A',
+        'drink'    => 'Shots',
+        'par'      => 3
+    ],
+    [
+        'location' => 'Pub B',
+        'drink'    => 'Water',
+        'par'      => 7
+    ]
+]);
+
+define('TEMP_players', [
+    [
+        'nickname'    => 'Jess',
+        'scores'      => [2, 3],
+        'score_total' => 5
+    ],
+    [
+        'nickname'    => 'Jill',
+        'scores'      => [3, 5],
+        'score_total' => 8
+    ],
+    [
+        'nickname'    => 'Bob',
+        'scores'      => [1, 8],
+        'score_total' => 9
+    ],
+    [
+        'nickname'    => 'Mark',
+        'scores'      => [4, 7],
+        'score_total' => 11
+    ],
+]);
+
 Route::view('/', 'home');
+
+Route::post('/manage', function () {
+
+    // Create new unique game
+    // Create new player for game using nickname and assign them as admin
+
+    return view('manage');
+});
 
 Route::get('/manage/{game_code}', function ($gameCode) {
 
     $tempData = [
-        'game_code' => $gameCode
+        'game_code'   => $gameCode,
+        'game_name'   => 'Test game name',
+        'max_players' => 10,
+        'holes'       => TEMP_holes,
+        'players'     => TEMP_players
     ];
 
     return view('manage', $tempData);
 });
 
+Route::post('/play', function () {
+
+    // Check game exists
+    // Create player for game from nickname if doesnt exist
+    // Load existing if does
+    // Goto play page
+
+    return view('play');
+});
+
 Route::get('/play/{game_code}', function ($gameCode) {
 
     $tempData = [
-        'holes'     => [
-            [
-                'name'  => 'Pub A',
-                'par'   => 3
-            ],
-            [
-                'name'  => 'Pub B',
-                'par'   => 7
-            ]
-        ],
+        'game_name'    => 'Test game name',
         'current_hole' => 1,
         'par_total'    => 10,
-        'players'      => [
-            [
-                'nickname'    => 'Jess',
-                'scores'      => [2, 3],
-                'score_total' => 5
-            ],
-            [
-                'nickname'    => 'Jill',
-                'scores'      => [3, 5],
-                'score_total' => 8
-            ],
-            [
-                'nickname'    => 'Bob',
-                'scores'      => [1, 8],
-                'score_total' => 9
-            ],
-            [
-                'nickname'    => 'Mark',
-                'scores'      => [4, 7],
-                'score_total' => 11
-            ],
-        ]
+        'holes'        => TEMP_holes,
+        'players'      => TEMP_players
     ];
 
     return view('play', $tempData);
