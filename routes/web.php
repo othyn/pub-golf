5414fc5dc77620697849af4fc94869214ab58e63
Ben <github@othyn.com>
2018-03-25 19:55:19 +0100

    Temporary data feed while prototyping page

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,46 @@
 Route::get('/', function () {
     return view('join');
 });
+
+Route::get('/play', function () {
+
+    $tempData = [
+        'game_code' => 'ABCDEFG',
+        'holes'     => [
+            [
+                'name'  => 'Pub A',
+                'par'   => 3
+            ],
+            [
+                'name'  => 'Pub B',
+                'par'   => 7
+            ]
+        ],
+        'current_hole' => 1,
+        'par_total'    => 10,
+        'players'      => [
+            [
+                'nickname'    => 'Jess',
+                'scores'      => [2, 3],
+                'score_total' => 5
+            ],
+            [
+                'nickname'    => 'Jill',
+                'scores'      => [3, 5],
+                'score_total' => 8
+            ],
+            [
+                'nickname'    => 'Bob',
+                'scores'      => [1, 8],
+                'score_total' => 9
+            ],
+            [
+                'nickname'    => 'Mark',
+                'scores'      => [4, 7],
+                'score_total' => 11
+            ],
+        ]
+    ];
+
+    return view('play', $tempData);
+});
