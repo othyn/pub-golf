e745113d1155604c485094011038aeadc7f1b845
Ben <github@othyn.com>
2018-03-26 23:41:54 +0100

    Init Player model
    Allow name to be mass fillable, hook into the boot method for the model
    to allow for automatic assignment of UUID field upon create and define
    relationship to PlayerScore

--- app/Player.php
+++ app/Player.php
@@ -4,7 +4,38 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Player extends Model
-{
-    //
+use Uuid;
+
+use App\PlayerScore;
+
+class Player extends Model {
+
+    /**
+     * Whitelist of fields to allow mass assignment
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * The "booting" method of the model.
+     * @return void
+     */
+    public static function boot() {
+
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->uuid = (string) Uuid::generate(4);
+        });
+        // Auto generate UUID into uuid DB field upon create
+    }
+
+    /**
+     * Player relationship to PlayerScore
+     * @return Collection   PlayerScores attached to the player
+     */
+    public function scores() {
+
+        return $this->hasMany(PlayerScore::class);
+    }
 }
