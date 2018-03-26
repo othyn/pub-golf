c041d15b8a1d701c0ccd609e94fc99cb1e701b59
Ben <github@othyn.com>
2018-03-26 23:39:36 +0100

    Game model init
    Set route key, define relationship with holes, players & scores and
    allow for the creation of a player against a game

--- app/Game.php
+++ app/Game.php
@@ -4,7 +4,55 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Game extends Model
-{
-    //
+use App\Hole;
+use App\Player;
+use App\PlayerScore;
+
+class Game extends Model {
+
+    /**
+     * DOCS: Route Model Binding > Implicit Binding > Customizing The Key Name
+     * Allows overriding of the default database column to use on binding instead of 'id'
+     * @return string   DB column name
+     */
+    public function getRouteKeyName() {
+
+        return 'game_code';
+    }
+
+    /**
+     * Game relationship to Hole
+     * @return Collection   Holes attached to the game
+     */
+    public function holes() {
+
+        return $this->hasMany(Hole::class);
+    }
+
+    /**
+     * Game relationship to Player
+     * @return Collection   Players attached to the game
+     */
+    public function players() {
+
+        return $this->hasMany(Player::class);
+    }
+
+    /**
+     * Game relationship to PlayerScore
+     * @return Collection   PlayerScores attached to the game
+     */
+    public function playerScores() {
+
+        return $this->hasMany(PlayerScore::class);
+    }
+
+    /**
+     * Add a player to a game
+     * @param string $name player name
+     */
+    public function addPlayer($name) {
+
+        $this->players()->create(compact('name'));
+    }
 }
