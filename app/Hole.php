6ddb12a8313beba4fb3ae97fc8e53068a44a2391
Ben <github@othyn.com>
2018-03-27 23:27:10 +0100

    Hole relationships

--- app/Hole.php
+++ app/Hole.php
@@ -4,7 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Hole extends Model
-{
-    //
+use App\Game;
+use App\PlayerScore;
+
+class Hole extends Model {
+
+    /**
+     * Hole relationship to Game
+     * @return Collection   Parent Game for the Hole
+     */
+    public function game() {
+
+        return $this->belongsTo(Game::class);
+    }
+
+    /**
+     * Hole relationship to PlayerScore
+     * @return Collection   PlayerScores attached to the Hole
+     */
+    public function scores() {
+
+        return $this->hasMany(PlayerScore::class);
+    }
 }
