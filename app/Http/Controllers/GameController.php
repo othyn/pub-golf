d225d3fc8de75bcbab99e06d71a2c9dd3caf7b3d
Ben <github@othyn.com>
2018-03-27 21:39:32 +0100

    Refactor joining into Game

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,7 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class GameController extends Controller
-{
-    //
+use App\Game;
+
+class GameController extends Controller {
+
+    /**
+     * Handles game creation
+     * @return void
+     */
+    public function create() {
+
+        $this->validate(request(), [
+            'name'           => 'required|min:1|max:50',
+            'organiser_name' => 'required|min:1|max:50'
+        ]);
+
+        $game = Game::create([request('name')]);
+
+        $game->addPlayer(request('organiser_name'));
+
+        // Redirect to ManageController@index with game obj
+
+        return back();
+
+        //return view('manage', ['game_code' => $game->game_code]);
+    }
+
+    /**
+     * Verifies game exists, created a player for the game,
+     * then dumps the player at the scorecard
+     * @param  Game   $game Game model instance
+     * @return void
+     */
+    public function join(Game $game) {
+
+        $this->validate(request(), [
+            'name' => 'required|min:1|max:50'
+        ]);
+
+        $game->addPlayer(request('name'));
+
+        // Redirect to PlayController@index
+
+        return back();
+
+        //return view('play', ['game_code' => $game->game_code]);
+    }
 }
