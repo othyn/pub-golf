a6c7e2a5cfe60d3f022c77c21894a722459109ea
Ben <github@othyn.com>
2018-03-27 23:27:17 +0100

    PlayerScore relationships

--- app/PlayerScore.php
+++ app/PlayerScore.php
@@ -4,7 +4,36 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PlayerScore extends Model
-{
-    //
+use App\Game;
+use App\Hole;
+use App\Player;
+
+class PlayerScore extends Model {
+
+    /**
+     * PlayerScore relationship to Game
+     * @return Collection   Parent Game for the PlayerScore
+     */
+    public function game() {
+
+        return $this->belongsTo(Game::class);
+    }
+
+    /**
+     * PlayerScore relationship to Hole
+     * @return Collection   Parent Hole for the PlayerScore
+     */
+    public function hole() {
+
+        return $this->belongsTo(Hole::class);
+    }
+
+    /**
+     * PlayerScore relationship to Player
+     * @return Collection   Parent Player for the PlayerScore
+     */
+    public function player() {
+
+        return $this->belongsTo(Player::class);
+    }
 }
