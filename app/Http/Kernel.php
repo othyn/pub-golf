47a02bf8b895bf1a8805ba484e0685d31945d3f8
Ben <github@othyn.com>
2018-03-28 01:50:52 +0100

    Register middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,5 +59,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'user' => \App\Http\Middleware\MustBeUser::class,
+        'admin' => \App\Http\Middleware\MustBeAdmin::class,
     ];
 }
