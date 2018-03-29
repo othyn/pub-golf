73b92080f727461199528789832a21ca982d4c88
Ben <github@othyn.com>
2018-03-29 19:26:43 +0100

    flash data to join instead of home

--- resources/views/join.blade.php
+++ resources/views/join.blade.php
@@ -7,6 +7,8 @@
     <h1 class="title is-fancy-font is-size-0">Join a game</h1>
     <h4 class="subtitle p-b-md">Enter a nickname and get playing!</h4>
 
+    @include ('partials.flash-warning')
+
     <div class="tile is-child box content">
 
         <h2 class="is-fancy-font is-size-3">Join Game</h2>
@@ -21,7 +23,7 @@
 
             <div class="field">
                 <div class="control has-icons-left">
-                    <input class="input is-medium" type="text" name="name" placeholder="Your nickname" minlength="1" maxlength="50" required autocomplete="off">
+                    <input class="input is-medium" type="text" name="name" placeholder="Your nickname" minlength="1" maxlength="50" required value="@if($name = session('join.name')){{$name}}@endif">
                     <span class="icon is-small is-left">
                         <i class="fa fa-user fa-xs"></i>
                     </span>
