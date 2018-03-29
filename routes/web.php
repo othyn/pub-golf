d9cd8ef1d616b0aabdc632d34f6fa60671f74acf
Ben <github@othyn.com>
2018-03-29 01:35:23 +0100

    TEMP: increase throttle limit

--- routes/web.php
+++ routes/web.php
@@ -39,14 +39,14 @@ Route::get('/games/{game}/edit', 'GameController@edit')->name('game.edit')
  * AJAX Game routes
  */
 
-Route::middleware('user', 'throttle:100,1')->group(function () {
+Route::middleware('user', 'throttle:250,1')->group(function () {
 
     Route::get('/games/{game}/active-hole', 'GameController@activeHole');
     Route::get('/games/{game}/leaderboard', 'GameController@leaderboard');
 
 });
 
-Route::middleware('user', 'throttle:100,1')->group(function () {
+Route::middleware('user', 'throttle:250,1')->group(function () {
 
     Route::patch('/games/{game}/players/{player}/score', 'PlayerController@update');
 
